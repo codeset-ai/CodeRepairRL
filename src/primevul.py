cd263187e995809bb90a003f9944e2fc1f4aec9d
Bjarni <bjarnihauku11@gmail.com>
2025-02-18 15:00:49 +0100

    better hf dataset

--- src/primevul.py
+++ src/primevul.py
@@ -16,16 +16,19 @@ class PrimeVul(Dataset):
     
     URL = "https://drive.google.com/drive/folders/19iLaNDS0z99N8kB_jBRTmDLehwZBolMY"
     SPLITS = ("train", "valid", "test")
+    TYPES = ("paired", "unpaired")
     
-    def __init__(self, split:Literal["train", "valid", "test"], root:str="data"):
+    def __init__(self, split:Literal["train", "valid", "test"], dataset_type:Literal["paired", "unpaired"]="paired", root:str="data"):
         """Initialize PrimeVul dataset.
         
         Args:
             split: Dataset split ('train', 'val', or 'test')
+            dataset_type: Type of dataset ('paired', 'unpaired'), 'paired' being adjacent vuln/fix pairs
             root: Root directory for data
         """
         self.root = Path(root) / "PrimeVul"
         self.split = split
+        self.type = dataset_type
         self.raw_dir = self.root / "raw"
         self.processed_dir = self.root / "processed"
         
@@ -35,7 +38,7 @@ class PrimeVul(Dataset):
         self.data = self.load()
         
     def _check_exists(self) -> bool:
-        return (self.processed_dir / f"{self.split}.json").exists()
+        return (self.processed_dir / f"{self.split}_{self.type}.json").exists()
     
     # TODO: There is also a file_contents directory which contains the entire files instead of just the vulnerable snippets.
     #       We would need to create a mapping from hash to file since we need to search through all the folders for the file
@@ -53,26 +56,25 @@ class PrimeVul(Dataset):
         self.processed_dir.mkdir(parents=True, exist_ok=True)
         
         for split in PrimeVul.SPLITS:
-            file_name = f"primevul_{split}_paired.jsonl"
-            if not (self.raw_dir / file_name).exists():
-                continue
+            for type in PrimeVul.TYPES:
+                file_name = f"primevul_{split}" + (f"_{type}.jsonl" if type == 'paired' else ".jsonl")
                 
-            data = [json.loads(line) for line in open(self.raw_dir / file_name)]
-            
-            # Standardize CWE labels
-            data = [x if "cwe" in x else x | {"cwe": "Other"} for x in data]
-            data = [x | {"cwe": "Other"} if x["cwe"] == "" else x for x in data]
-            data = [x | {"cwe": "Other"} if isinstance(x["cwe"], list) and len(x["cwe"]) == 0 else x for x in data]
-            data = [x | {"cwe": x["cwe"][0]} if isinstance(x["cwe"], list) and len(x["cwe"]) == 1 else x for x in data]
-            data = [x | {"cwe": "Other"} if isinstance(x["cwe"], str) and not x["cwe"].startswith("CWE-") else x for x in data]
-            data = [x | {"cwe": [x["cwe"]]} if isinstance(x["cwe"], str) else x for x in data]
-            
-            # Save processed data
-            json.dump(data, open(self.processed_dir / f"{split}.json", "w"), indent=4)
+                data = [json.loads(line) for line in open(self.raw_dir / file_name)]
+                
+                # Standardize CWE labels
+                data = [x if "cwe" in x else x | {"cwe": "Other"} for x in data]
+                data = [x | {"cwe": "Other"} if x["cwe"] == "" else x for x in data]
+                data = [x | {"cwe": "Other"} if isinstance(x["cwe"], list) and len(x["cwe"]) == 0 else x for x in data]
+                data = [x | {"cwe": x["cwe"][0]} if isinstance(x["cwe"], list) and len(x["cwe"]) == 1 else x for x in data]
+                data = [x | {"cwe": "Other"} if isinstance(x["cwe"], str) and not x["cwe"].startswith("CWE-") else x for x in data]
+                data = [x | {"cwe": [x["cwe"]]} if isinstance(x["cwe"], str) else x for x in data]
+                
+                # Save processed data
+                json.dump(data, open(self.processed_dir / f"{split}_{type}.json", "w"), indent=4)
             
     def load(self) -> List[Dict[str, Any]]:
         """Load the processed data for the current split."""
-        file_path = self.processed_dir / f"{self.split}.json"
+        file_path = self.processed_dir / f"{self.split}_{self.type}.json"
         if not file_path.exists():
             raise FileNotFoundError(
                 f"Processed file {file_path} not found. "
@@ -106,15 +108,23 @@ if __name__ == "__main__":
 
     def convert(data):
         return [{
-            "func": item["func"],
-            "is_vulnerable": item["target"]==0,
-            "cwe": item["cwe"]
-        } for item in data]
+            "is_vulnerable": x["target"] == 0,
+            "func": x["func"],
+            "cwe": x["cwe"],
+            "project": x["project"],
+            "commit_id": str(x["commit_id"]),
+            "hash": str(x["hash"]),
+            "big_vul_idx": x.get("big_vul_idx", None),  # sometimes not present
+            "idx": x.get("idx", None),
+        } for x in data]
 
     hfds = DatasetDict({
-        "train": HFDataset.from_list(convert(PrimeVul("train").data)),
-        "valid": HFDataset.from_list(convert(PrimeVul("valid").data)),
-        "test": HFDataset.from_list(convert(PrimeVul("test").data))
+        "train_paired": HFDataset.from_list(convert(PrimeVul("train", "paired").data)),
+        "valid_paired": HFDataset.from_list(convert(PrimeVul("valid", "paired").data)),
+        "test_paired": HFDataset.from_list(convert(PrimeVul("test", "paired").data)),
+        "train_unpaired": HFDataset.from_list(convert(PrimeVul("train", "unpaired").data)),
+        "valid_unpaired": HFDataset.from_list(convert(PrimeVul("valid", "unpaired").data)),
+        "test_unpaired": HFDataset.from_list(convert(PrimeVul("test", "unpaired").data)),
     })
 
-    hfds.push_to_hub("Bobbi/Primevul")
+    hfds.push_to_hub("ASSERT-KTH/Primevul")
