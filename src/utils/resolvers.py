755e81edd4d1443567324389cf7b461e432a5e12
Bjarni <bjarnihauku11@gmail.com>
2025-03-22 21:59:01 +0100

    bf16 preferance

--- src/utils/resolvers.py
+++ src/utils/resolvers.py
@@ -21,4 +21,6 @@ def resolve_fp16(use_fp16=None):
     if use_fp16 is not None:
         return use_fp16
     # Use FP16 on Pascal+ GPUs (SM 6.0+) only if BF16 is not supported
-    return torch.cuda.is_available() and torch.cuda.get_device_capability(0)[0] >= 6 and torch.cuda.get_device_capability(0)[0] < 8 
+    bf16 = resolve_bf16()
+    fp16 = torch.cuda.is_available() and torch.cuda.get_device_capability(0)[0] >= 6 and torch.cuda.get_device_capability(0)[0] < 8 
+    return fp16 and not bf16  # if bf16 is supported, we don't use fp16
