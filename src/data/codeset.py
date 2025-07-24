837fc7807ebfe303bc15490dc7d59144f5c30eb8
Nfsaavedra <nuno.saavedra@tecnico.ulisboa.pt>
2025-07-24 21:02:41 +0100

    fix dataset format

--- src/data/codeset.py
+++ src/data/codeset.py
@@ -8,7 +8,8 @@ def get_codeset_dataset() -> Dataset:
         {
             "dataset": "gitbug-java",
             "sample-id": "assertj-assertj-vavr-f4d7f276e87c",
-            "task": "Some of the tests in this project are failing. Your task is to fix the source code such that all tests pass."
+            "task": "Some of the tests in this project are failing. Your task is to fix the source code such that all tests pass.",
+            "prompt": "Dummy" # Add a dummy "prompt" key for compatibility with trl
         }
     ]
     return Dataset.from_list(dataset)
